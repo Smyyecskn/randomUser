81ac958413deba4a1fb96d841aeda7b97526b881
smyye <smyyeoztrk43@gmail.com>
2024-03-24 18:07:05 +0300

    update

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,12 +6,12 @@ const Footer = () => {
   return (
     <div className="footer-div">
       <a
-        href="https://github.com/anthonyharold67"
+        href="https://github.com/Smyyecskn"
         target="_blank"
         rel="noopener noreferrer"
         style={{ textDecoration: "none" }}
       >
-        <h2 className="brand">{"Anthony"}</h2>
+        <h2 className="brand">{"Smyye"}</h2>
       </a>
       <img
         src={designSvg}
